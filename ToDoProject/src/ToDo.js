a83ab4589d59418a63073371149d263a96def8f1
Zade <muhderfannnn@gmail.com>
2024-06-23 21:05:06 +0800

    Successfully added event listener to create task button.

--- ToDoProject/src/ToDo.js
+++ ToDoProject/src/ToDo.js
@@ -1,84 +1,89 @@
 class ToDoObject {
-    
+	static ToDoObjectCount = 0;
 
-    static ToDoObjectCount = 0;
+	#title;
+	#description;
+	#dueDate;
+	#priority;
+	#notes;
+	#checklist;
+	#project;
 
-    #title;
-    #description;
-    #dueDate;
-    #priority;
-    #notes;
-    #checklist;
-    #project;
-    
-    constructor(title, description, dueDate, priority, notes, checklist, project) {
-        this.#title = title;
-        this.#description = description;
-        this.#dueDate = dueDate;
-        this.#priority = priority;
-        this.#notes = notes;
-        this.#checklist = checklist;
-        this.#project = project;
-    }
+	constructor(
+		title,
+		description,
+		dueDate,
+		priority,
+		notes,
+		checklist,
+		project
+	) {
+		this.#title = title;
+		this.#description = description;
+		this.#dueDate = dueDate;
+		this.#priority = priority;
+		this.#notes = notes;
+		this.#checklist = checklist;
+		this.#project = project;
+	}
 
-    // Getters
-    getTitle() {
-        return this.#title;
-    }
+	// Getters
+	getTitle() {
+		return this.#title;
+	}
 
-    getDescription() {
-        return this.#description;
-    }
+	getDescription() {
+		return this.#description;
+	}
 
-    getDueDate() {
-        return this.#dueDate;
-    }
+	getDueDate() {
+		return this.#dueDate;
+	}
 
-    getPriority() {
-        return this.#priority;
-    }
+	getPriority() {
+		return this.#priority;
+	}
 
-    getNotes() {
-        return this.#notes;
-    }
+	getNotes() {
+		return this.#notes;
+	}
 
-    getChecklist() {
-        return this.#checklist;
-    }
+	getChecklist() {
+		return this.#checklist;
+	}
 
-    getCategory() {
-        return this.#project;
-    }
+	getCategory() {
+		return this.#project;
+	}
 
-    // Setters
-    setTitle(title) {
-        this.#title = title;
-    }
+	// Setters
+	setTitle(title) {
+		this.#title = title;
+	}
 
-    setDescription(description) {
-        this.#description = description;
-    }
+	setDescription(description) {
+		this.#description = description;
+	}
 
-    setDueDate(dueDate) {
-        this.#dueDate = dueDate;
-    }
+	setDueDate(dueDate) {
+		this.#dueDate = dueDate;
+	}
 
-    setPriority(priority) {
-        this.#priority = priority;
-    }
+	setPriority(priority) {
+		this.#priority = priority;
+	}
 
-    setNotes(notes) {
-        this.#notes = notes;
-    }
+	setNotes(notes) {
+		this.#notes = notes;
+	}
 
-    setChecklist(checklist) {
-        this.#checklist = checklist;
-    }
+	setChecklist(checklist) {
+		this.#checklist = checklist;
+	}
 
-    setCategory(project) {
-        this.#project = project;
-    }
-
+	setCategory(project) {
+		this.#project = project;
+	}
 }
 
 // Add methods to change each property in ToDoObject?
@@ -86,4 +91,25 @@ class ToDoObject {
 // Add get and setters to classes
 // Install the date pack so that formatting date is easier.
 
-export default ToDoObjectClass; 
+// Get ToDoObject values from create new task form
+export function createToDoObject() {
+	const toDoTitle = document.getElementById("floatingTitle").value;
+	const toDoDescription = document.getElementById("floatingDescription").value;
+	const toDoDate = document.getElementById("floatingDate").value;
+	const toDoTime = document.getElementById("floatingTime").value;
+	const toDoPriority = document.getElementById("todoPriority").value;
+
+	const ToDoObject1 = new ToDoObject(
+		toDoTitle,
+		toDoDescription,
+		toDoDate,
+		toDoPriority,
+		"",
+		"",
+		""
+	);
+
+	return ToDoObject1;
+}
+
+export default ToDoObject;
--- ToDoProject/src/index.js
+++ ToDoProject/src/index.js
@@ -1,9 +1,16 @@
-import styles from './styles/styles.css'
-import * as boostrap from 'bootstrap'
-
-
+import styles from "./styles/styles.css";
+import * as boostrap from "bootstrap";
+import * as ToDo from "./ToDo.js";
 // Task is under project.
 // Find a way to display the projects at the navbar.
-function AddTasksFromHTMLMODAL(){
-    
+
+// Add Event Listeners
+
+const createToDoObjectBtn = document.getElementById("btn_CreateToDo");
+createToDoObjectBtn.addEventListener("click", handleToDoObjBtn);
+
+function handleToDoObjBtn() {
+	const data = ToDo.createToDoObject();
+	console.log("hello");
+	console.log(data);
 }
